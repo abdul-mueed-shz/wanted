2d62df0d41d9f771c2bfc486877d31364264756a
Äbdul Mueed Shahbaz <abdulmueedshahbaz@gmail.com>
2024-06-09 02:35:47 +0500

    first commit

---
+++ src/components/main/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App-background">
+      <div className="App-header">
+        <p className="anton-font" style={{ fontSize: "201px" }}>
+          Wanted
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
