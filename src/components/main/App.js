ef34dcb29952b03d3e69ab3ff69bcbdc2cc15805
Äbdul Mueed Shahbaz <abdulmueedshahbaz@gmail.com>
2024-06-11 01:21:58 +0500

    progress

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,20 +1,27 @@
 import "./App.css";
-
+import profilePicture from "../../assets/images/user/profilePicture.jpg";
 function App() {
   return (
     <div className="App-background">
       <div className="App-header">
-        <p className="anton-font" style={{ fontSize: "201px" }}>
-          Wanted
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1 className="anton-font text-241px">WANTED</h1>
+      </div>
+      <div>
+        <div
+          className="flex justify-center pt-16"
+          style={{ borderTop: "2px dashed" }}
         >
-          Learn React
-        </a>
+          <div className="relative">
+            <img
+              src={profilePicture}
+              alt=""
+              style={{ maxWidth: "340px" }}
+            ></img>
+            <div className="text-2xl font-medium anton-font p-2 max-w-210px border-radius__3pxSolidAppGray rounded-xl absolute -top-1.25rem -right-1.875rem">
+              Available for work
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "210px": "13.125rem", // Custom width for 210px in rem
+        "330px": "20.625rem", // Custom width for 330px in rem
+      },
+      maxWidth: {
+        "210px": "13.125rem", // Custom width for 210px in rem
+        "330px": "20.625rem", // Custom width for 330px in rem
+      },
+      fontSize: {
+        "241px": "15.0625rem", // Custom size for 241px in rem
+      },
+      inset: {
+        "-1.875rem": "-1.875rem", // Custom left and right value for -30px in rem
+        "-1.25rem": "-1.25rem", // Custom left and right value for -20px in rem
+      },
+    },
   },
   plugins: [],
 };
